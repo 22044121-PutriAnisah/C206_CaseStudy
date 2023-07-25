6c0f8d44ff4b9fb46dc235603d773b4d62004469
22024357 <22024357@LAPTOP-3OADV02L>
2023-07-25 16:30:29 +0800

    currency

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -4,6 +4,7 @@ public class C206_CaseStudy {
 	public static void main(String[] args) {
 		ArrayList<User> userList = new ArrayList<User>();
 		User user1 = new User("Alan Tan","alantan@abc.com","Alantan_123",80081234);
+		userList.add(user1);
 		
 		
 		
@@ -12,6 +13,13 @@ public class C206_CaseStudy {
 		
 		ArrayList<admin> adminList = new ArrayList<admin>();
 		admin admin1 = new admin("Joshua Lim","JoshLim_123");
+		adminList.add(admin1);
+		
+		
+		currency[] currencyArr = new currency[100];
+		currency c1 = new currency("Singapore","SGD");
+		
+		currencyArr[0] = c1;
 		
 
 	}
---
+++ C206_CaseStudy/src/currency.java
@@ -0,0 +1,14 @@
+
+public class currency {
+	private String currencies;
+	private String country;
+	
+	
+	public currency(String country,String currencies) {
+		this.country = country;
+		this.currencies = currencies;
+		
+		
+	}
+
+}
