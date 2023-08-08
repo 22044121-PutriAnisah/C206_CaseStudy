1604afc314181a740bbfb83eeab067d48b125b4f
22044121-PutriAnisah <134935066+22044121-PutriAnisah@users.noreply.github.com>
2023-08-08 22:44:10 +0800

    Update feedback.java

--- C206_CaseStudy/src/feedback.java
+++ C206_CaseStudy/src/feedback.java
@@ -1,12 +1,49 @@
+	import java.time.LocalDate;
 
-public class feedback {
-	private String comment;
-	
-	public feedback(String comment) {
-		this.comment = comment;
+	public class feedback {
+		private int id;
+		private String feedback;
+		private LocalDate date;
+		private String comments;
+		
+		public feedback(int id, String feedback, LocalDate date, String comments) {
+			this.id = id;
+			this.feedback = feedback;
+			this.date = date;
+			this.comments = comments;
+		}
+		
+		public int getId() {
+			return id;
+		}
+		
+		public void setId(int yes) {
+			this.id = yes;
+		}
+		
+		public String getfeedback() {
+			return feedback;
+		}
+		
+		public void setfeedback(String edit) {
+			this.feedback = edit;
+		}
+		
+		public LocalDate getDate() {
+			return date;
+		}
+		
+		public String getComment() {
+			return comments;
+		}
+		
+		public void setComment(String comme) {
+			this.comments = comme;
+		}
+		
+		public String toString() {
+			String itemInfo = String.format("%-10s %-20s %-20s %-10s", id, feedback, date, comments);
+			return itemInfo;
+			
+		}
 	}
-	
-	public String getComment() {
-		return comment;
-	}
-}
