869d5fa9534be38c0f3fb2501fa907da54179829
22044121-PutriAnisah <134935066+22044121-PutriAnisah@users.noreply.github.com>
2023-08-08 22:42:18 +0800

    Update User.java
    added role

--- C206_CaseStudy/src/User.java
+++ C206_CaseStudy/src/User.java
@@ -1,41 +1,57 @@
-
-
-
-
-/**
- * 
- */
-
-/**
- * @author 22044121
- *
- */
 public class User {
+	private int userId;
+	private static String role;
 	private String name;
 	private String email;
 	private String password;
 	private int phoneNum;
-
-	public User(String name, String email, String password,int phoneNum) {
+	
+	public User(int userId, String role, String name, String email, String password, int phoneNum) {
 		super();
+		this.userId = userId;
+		this.role = role;
 		this.name = name;
 		this.email = email;
 		this.password = password;
 		this.phoneNum = phoneNum;
 	}
 
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	
+	public static String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
 	public String getName() {
 		return name;
 	}
-	public String getEmail() {
-		return email;
-	}
-	public String getPassword() {
-		return password;
-	}	
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
@@ -48,6 +64,6 @@ public class User {
 		this.phoneNum = phoneNum;
 	}
 	
-	
 
 }
+
